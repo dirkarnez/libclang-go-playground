e9c102767cb764063761b0410da710552a05db4b
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-06 14:44:58 +0800

    - add main.go

--- main.go
+++ main.go
@@ -1,182 +1,57 @@
 package main
 
+/*
+#cgo LDFLAGS: -lclang
+
+#include <clang-c/Index.h>
+#include <stdlib.h> // For free
+
+// Wrapper function to parse a C++ file using libclang
+CXTranslationUnit parse_cpp_file_c(const char* filepath) {
+    CXIndex index = clang_createIndex(0, 0);
+    CXTranslationUnit tu = clang_parseTranslationUnit(
+        index,
+        filepath,
+        NULL, 0, // command line arguments
+        NULL, 0, // unsaved files
+        CXTranslationUnit_None
+    );
+    // clang_disposeIndex(index); // Don't dispose until tu is no longer needed
+    return tu;
+}
+
+// Example to get a cursor from a translation unit
+CXCursor get_translation_unit_cursor(CXTranslationUnit tu) {
+    return clang_getTranslationUnitCursor(tu);
+}
+
+// Function to dispose the translation unit
+void dispose_translation_unit_c(CXTranslationUnit tu) {
+    clang_disposeTranslationUnit(tu);
+}
+*/
+import "C"
 import (
-	"bufio"
-	"context"
-	"flag"
 	"fmt"
-	"io/fs"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
-
-	"github.com/chromedp/cdproto/network"
-	"github.com/chromedp/cdproto/runtime"
-	"github.com/chromedp/chromedp"
-	"github.com/graniticio/inifile"
-)
-
-var (
-	dir string
+	"unsafe"
 )
 
 func main() {
-	flag.StringVar(&dir, "dir", "", "Absolute path for target directory")
+	filepath := "my_cpp_file.cpp" // Replace with your C++ file path
 
-	flag.Parse()
-	if len(dir) < 1 {
-		log.Fatal("No --dir is given")
+	cFilepath := C.CString(filepath)
+	defer C.free(unsafe.Pointer(cFilepath))
+
+	tu := C.parse_cpp_file_c(cFilepath)
+	if tu == nil {
+		fmt.Println("Failed to parse C++ file.")
+		return
 	}
+	defer C.dispose_translation_unit_c(tu)
 
-	urlFiles := Scan(dir, ".url")
-	urlFilesLen := len(urlFiles)
-	if urlFilesLen < 1 {
-		log.Fatal("No .url file found")
-	}
-	fmt.Printf("There are %d url files\n", len(urlFiles))
+	// Now you can work with the translation unit and its cursor to extract information
+	cursor := C.get_translation_unit_cursor(tu)
+	fmt.Printf("Parsed C++ file: %s (Cursor kind: %d)\n", filepath, cursor.kind)
 
-	file, err := os.Create(fmt.Sprintf("%s.txt", getFolderName(dir)))
-	errExit(err)
-	defer file.Close()
-	w := bufio.NewWriter(file)
-
-	for _, s := range urlFiles {
-		ic, err := inifile.NewIniConfigFromPath(s)
-		errExit(err)
-		url, err := ic.Value("InternetShortcut", "URL")
-		errExit(err)
-		fmt.Println("checking", url, ", in", s, "...")
-		protocol := url[0:strings.Index(url, `://`)]
-		if protocol == `http` || protocol == `https` {
-			title, err := getTitle(url)
-			errExit(err)
-			fmt.Fprintf(w, "- [%s](%s)\n", title, url)
-		} else {
-			fmt.Fprintf(w, "- [%s](%s)\n", url, url)
-		}
-	}
-	errExit(w.Flush())
-
-	// for _, s := range urlFiles {
-	// 	errExit(os.Remove(s))
-	// }
-}
-
-func errExit(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func Scan(root, ext string) []string {
-	var a []string
-	filepath.WalkDir(root, func(s string, d fs.DirEntry, e error) error {
-		if e != nil {
-			return e
-		}
-		if filepath.Ext(d.Name()) == ext {
-			a = append(a, s)
-		}
-		return nil
-	})
-	return a
-}
-
-func getTitle(urlstr string) (string, error) {
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
-	var title string
-
-	chromedp.ListenTarget(ctx, func(ev interface{}) {
-
-		switch ev := ev.(type) {
-
-		case *network.EventResponseReceived:
-			resp := ev.Response
-			if resp.URL == urlstr {
-				log.Printf("received headers: %s %s", resp.URL, resp.MimeType)
-				if resp.MimeType != "text/html" {
-					chromedp.Cancel(ctx)
-				}
-
-				if strings.Contains(resp.URL, "youtube.com") {
-					log.Printf("YT!!")
-				}
-
-				// may be redirected
-				switch ContentType := resp.Headers["Content-Type"].(type) {
-				case string:
-					// here v has type T
-					if !strings.Contains(ContentType, "text/html") {
-						chromedp.Cancel(ctx)
-					}
-				}
-
-				switch ContentType := resp.Headers["content-type"].(type) {
-				case string:
-					// here v has type T
-					if !strings.Contains(ContentType, "text/html") {
-						chromedp.Cancel(ctx)
-					}
-				}
-			}
-		}
-	})
-
-	req := `
-(async () => new Promise((resolve, reject) => {
-	var handle = NaN;
-
-	(function animate() {
-		if (!isNaN(handle)) {
-			clearTimeout(handle);
-		}
-
-		if (document.title.length > 0 && !document.title.startsWith("http")) {
-			resolve(document.title);
-		} else {
-			handle = setTimeout(animate, 1000);
-		}
-	}());
-}));
-`
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(urlstr),
-		//chromedp.Evaluate(`window.location.href`, &res),
-		chromedp.Evaluate(req, nil, func(p *runtime.EvaluateParams) *runtime.EvaluateParams {
-			return p.WithAwaitPromise(true)
-		}),
-		chromedp.Title(&title),
-	)
-	if err == context.Canceled {
-		// url as title
-		log.Printf("Cancel!!")
-		return urlstr, nil
-	}
-
-	return title, err
-}
-
-// fmt.Println(getFolderName(`P`))           //P
-// fmt.Println(getFolderName(`P:`))          //P
-// fmt.Println(getFolderName(`P:\`))         //P
-// fmt.Println(getFolderName(`P:\testing`))  //testing
-// fmt.Println(getFolderName(`P:\testing\`)) //testing
-func getFolderName(input string) string {
-	var folderName string
-	lastIndex := strings.LastIndex(input, `\`)
-	length := len(input)
-	if lastIndex+1 == length {
-		lastIndex = strings.LastIndex(input[0:length-1], `\`)
-		folderName = input[lastIndex+1 : length-1]
-	} else {
-		folderName = input[lastIndex+1 : length]
-	}
-
-	if folderName[len(folderName)-1:] == ":" {
-		return folderName[0 : len(folderName)-1]
-	} else {
-		return folderName
-	}
+	// Further processing of the AST using libclang functions via cgo
 }
